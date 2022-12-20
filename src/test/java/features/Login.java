0b86a6941206a33ebf6fd4fc1487ec32fee6dbbc
Harish hj <harish.hj@kapiva.in>
2022-12-20 20:20:10 +0530

    updated class

--- src/test/java/features/Login.java
+++ src/test/java/features/Login.java
@@ -7,19 +7,39 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import lombok.SneakyThrows;
 import resources.BaseTest;
 import resources.Em_BaseTest;
 import resources.SlackTest;
 
-public class Login extends Em_BaseTest{
+public class Login {
 	SlackTest slack=new SlackTest();
+	
+	@SneakyThrows
 	@Test
 	public void LoginTest() throws InterruptedException, IOException {
 		
+		DesiredCapabilities cap=new DesiredCapabilities();
+		
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+		
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+		
+		cap.setCapability("uiautomater2ServerInstallTimeout", 60000);
+		cap.setCapability("autoGrantPermissions", true);
+		cap.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/app-release.apk");
+		AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), cap);
+		
+//		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+//		driver=new AndroidDriver(url,cap);
 		SlackTest.Sendmessage("*******Kat*******");
 
 	    //driver.findElement(AppiumBy.xpath("//android.widget.Button[@index='0']")).click();
