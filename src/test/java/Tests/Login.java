e60ad2aaa013612328ae2150fc0b4956c1484a35
Harish hj <harish.hj@kapiva.in>
2023-01-06 16:39:53 +0530

    Updated

--- src/test/java/Tests/Login.java
+++ src/test/java/Tests/Login.java
@@ -2,15 +2,21 @@ package Tests;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import Res.Slacky;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -64,4 +70,41 @@ public class Login {
         }else {
         	System.out.println("Failed to Login");
         }      
+        driver.findElement(AppiumBy.accessibilityId("Home")).click();
+        Thread.sleep(2000);
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[@index=\"2\"]")).click();
+        
+       WebElement search = driver.findElement(AppiumBy.xpath("//android.widget.EditText[@index=\"1\"]"));
+       search.sendKeys("GetSlim juice");
+       driver.hideKeyboard();
+       
+       
+       
+       driver.findElement(AppiumBy.xpath("//android.widget.ImageView[@index=\"3\"]")).click();
+       
+       Thread.sleep(4000);
+       driver.findElement(AppiumBy.accessibilityId("BUY NOW")).click();
+        
+        Thread.sleep(3000);
+        System.out.println("BuyNowTest_PASS");
+        Slacky.Sendmessage("BuyNowTest                                   PASS");
+        
+        Thread.sleep(3000);
+    	WebElement el04=driver.findElement(By.xpath("//android.view.View"));
+    	int center_x1=el04.getRect().x + (el04.getSize().width/2);
+    	double start_y1=el04.getRect().y + (el04.getSize().height*0.6);
+    	double end_y1=el04.getRect().y + (el04.getSize().height*0.1);
+    	PointerInput finger2=new PointerInput(PointerInput.Kind.TOUCH,"finger2");
+    	
+    	Sequence swipe2=new Sequence(finger2, (int) 0.5);
+    	swipe2.addAction(finger2.createPointerMove(Duration.ofSeconds(0), PointerInput.Origin.viewport(), center_x1, (int)start_y1));
+    	swipe2.addAction(finger2.createPointerDown(0));
+    	swipe2.addAction(finger2.createPointerMove(Duration.ofMillis(400), PointerInput.Origin.viewport(), center_x1, (int)end_y1));
+    	swipe2.addAction(finger2.createPointerUp(0));
+   	    driver.perform(Arrays.asList(swipe2));
+        Thread.sleep(4000);
+   	    
+	 	driver.findElement(AppiumBy.xpath("//android.view.View[@content-desc=\"SAVE5\nGet extra 5% off on orders"
+	 			+ " above ₹989\"]")).click();
+	 	
 }   }
