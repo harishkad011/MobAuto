866b1b43234aa30ecefd8b2bbce1b251831e2d0b
Harish hj <harish.hj@kapiva.in>
2023-01-06 16:09:58 +0530

    Updated

--- src/test/java/Tests/Login.java
+++ src/test/java/Tests/Login.java
@@ -21,32 +21,26 @@ public class Login {
 	
 	public AndroidDriver driver;
 	
-	@BeforeMethod
-	public void Setup() throws MalformedURLException {
-	
-	DesiredCapabilities cap=new DesiredCapabilities();
-	
-	cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-	
-	cap.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-	//cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel");
-	//cap.setCapability(MobileCapabilityType.UDID, "72c414f1");
-	cap.setCapability("adbExecTimeout", 50000);
-	cap.setCapability("appWaitforLaunch", false);
-	cap.setCapability("appWaitDuration", 30000);
-	cap.setCapability("uiautomater2ServerInstallTimeout", 60000);
-	cap.setCapability("ignoreHiddenApiPolicyError", true);
-	cap.setCapability("autoGrantPermissions", true);
-	cap.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/App/app-release.apk");
-	AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-	
 	@SneakyThrows
 	@Test
 	public void LoginTest() throws InterruptedException, IOException {
 		
-
+		DesiredCapabilities cap=new DesiredCapabilities();
+		
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+		//cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel");
+		//cap.setCapability(MobileCapabilityType.UDID, "72c414f1");
+		cap.setCapability("adbExecTimeout", 50000);
+		cap.setCapability("appWaitforLaunch", false);
+		cap.setCapability("appWaitDuration", 30000);
+		cap.setCapability("uiautomater2ServerInstallTimeout", 60000);
+		cap.setCapability("ignoreHiddenApiPolicyError", true);
+		cap.setCapability("autoGrantPermissions", true);
+		cap.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/App/app-release.apk");
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	
 		WebElement cl=driver.findElement(AppiumBy.accessibilityId("Profile"));
         cl.click();
 	
@@ -63,32 +57,11 @@ public class Login {
         driver.findElement(AppiumBy.accessibilityId("Profile")).click();
         String logintext = driver.findElement(AppiumBy.accessibilityId("Harish QA")).getAttribute("content-desc");
        
-        
         Assert.assertEquals("Harish QA",logintext );
         if(logintext.equalsIgnoreCase("Harish QA")){
         	System.out.println("Logged in Successfully");
         	//SlackTest.Sendmessage("Logged in Successfully");
         }else {
         	System.out.println("Failed to Login");
-        } }
-        
-        @Test
-        public void BuyNowTest() throws InterruptedException {
-        	Thread.sleep(2000);
-            driver.findElement(AppiumBy.xpath("//android.widget.Button[@index=\"2\"]")).click();
-            
-           WebElement search = driver.findElement(AppiumBy.xpath("//android.widget.EditText[@index=\"1\"]"));
-           search.sendKeys("GetSlim juice");
-           driver.hideKeyboard();
-           
-           
-           
-           driver.findElement(AppiumBy.xpath("//android.widget.ImageView[@index=\"3\"]")).click();
-           
-           Thread.sleep(4000);
-           driver.findElement(AppiumBy.accessibilityId("BUY NOW")).click();
-            
-            Thread.sleep(3000);
-            System.out.println("BuyNowTest_PASS");
-  }
-}
+        }      
+}   }
